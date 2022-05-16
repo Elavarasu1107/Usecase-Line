8e110940776bbc6862c740fefc9442cf5dc3165b
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-05-16 17:30:00 +0530

    [Elavarasu] UC1-Linelength

---
+++ Usecase-Line/Usecase-Line/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UseCase_Line
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            Linecheck length = new Linecheck();
+            double lengthofLine1 = length.Coordinates(5, 8, 5, 18);
+            Console.WriteLine("Length of line1 is:" + lengthofLine1);
+        }
+    }
+}
