94b199aa8ecb6335d454e2425e73474d13b05134
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-05-16 17:40:32 +0530

    [Elavarasu] UC2-LineEquality

--- Usecase-Line/Usecase-Line/Program.cs
+++ Usecase-Line/Usecase-Line/Program.cs
@@ -9,7 +9,11 @@ namespace UseCase_Line
             Console.WriteLine("Welcome to Line Comparison Computation Program");
             Linecheck length = new Linecheck();
             double lengthofLine1 = length.Coordinates(5, 8, 5, 18);
+            double lengthofLine2 = length.Coordinates(5, 8, 5, 19);
             Console.WriteLine("Length of line1 is:" + lengthofLine1);
+            Console.WriteLine("Length of line2 is:" + lengthofLine2);
+
+            Console.WriteLine("Is given two lines are equal?:" + " " + lengthofLine1.Equals(lengthofLine2));
         }
     }
 }
