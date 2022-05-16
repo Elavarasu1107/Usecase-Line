c214ea882bdf81a47680ac9c3a049783fcd53e90
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-05-16 21:00:14 +0530

    [Elavarasu] LineCheckMethod2

---
+++ Usecase-LineMethod2/Usecase-LineMethod2/LinecheckMethod2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Usecase_LineMethod2
+{
+    internal class LinecheckMethod2
+    {
+        public void Coordinates()
+        {
+            double[] length = new double[2];
+
+            for(int i=0;i<2;i++)
+            {
+                Console.WriteLine("Enter value X1");
+                double x1=Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter value Y1");
+                double y1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter value X2");
+                double x2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter value Y2");
+                double y2 = Convert.ToDouble(Console.ReadLine());
+
+                length[i]=Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            }
+            Console.WriteLine("Length of line1 is:" + length[0]);
+            Console.WriteLine("Length of line2 is:" + length[1]);
+
+            Console.WriteLine("Is given two lines are equal?:" + " " + length[0].Equals(length[1]));
+
+            int compareLine = length[0].CompareTo(length[1]);
+
+            if (compareLine > 0)
+            {
+                Console.WriteLine("Line 1 is bigger than Line 2");
+            }
+            else if (compareLine < 0)
+            {
+                Console.WriteLine("Line 1 is smaller than Line 2");
+            }
+            else
+            {
+                Console.WriteLine("Line 1 is equal to Line 2");
+            }
+        }
+    }
+}
---
+++ Usecase-LineMethod2/Usecase-LineMethod2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Usecase_LineMethod2
+{
+    internal class Line
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+
+            LinecheckMethod2 methodCall=new LinecheckMethod2();
+            methodCall.Coordinates();
+        }
+    }
+}
